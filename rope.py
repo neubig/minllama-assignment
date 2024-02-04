891ef39dd37141c0a414fe71498737e9caa819d9
Vijay Viswanathan <vcomeinpeace@gmail.com>
2024-02-03 19:52:19 -0500

    Add comment to explain current implementation in rope.py

--- rope.py
+++ rope.py
@@ -59,6 +59,8 @@ def apply_rotary_emb(
     # reshape xq and xk to match the complex representation
     query_real, query_imag = query.float().reshape(query.shape[:-1] + (-1, 2)).unbind(-1)
     key_real, key_imag = key.float().reshape(key.shape[:-1] + (-1, 2)).unbind(-1)
+    # This separates each query/key vector into its odd and even indices (assuming *one-indexing*).
+    # query_real contains q_1, q_3, q_5, ... and query_imag contains q_2, q_4, q_6, ...
 
     # First, compute the trigonometric values in the second and fourth columns in
     # slide 22 (linked above).
